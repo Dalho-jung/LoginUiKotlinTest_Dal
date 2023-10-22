a8b904402fbc36a060e77d8db7f6a748b9d432da
Daljung <msfall@hanmail.net>
2023-10-22 11:22:04 +0900

    로그인처리까지

--- app/src/main/java/com/n2isys/loginuikotlintest_dal/MainActivity.kt
+++ app/src/main/java/com/n2isys/loginuikotlintest_dal/MainActivity.kt
@@ -2,6 +2,7 @@ package com.n2isys.loginuikotlintest_dal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.n2isys.loginuikotlintest_dal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,20 @@ class MainActivity : AppCompatActivity() {
         var viewBind = ActivityMainBinding.inflate(layoutInflater,null,false)
 
         setContentView(viewBind.root)
-        
+
+        viewBind.BtnLogin.setOnClickListener {
+
+            val ls_id = viewBind.TxtId.text
+            val ls_pwd = viewBind.TxtPwd.text
+
+            if(ls_id == "admin@test.com" && ls_pwd = "qwer"){
+                Toast.makeText("로그인성공", "", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText("로그인실패", "", Toast.LENGTH_SHORT).show()
+
+            }
+
+
+        }
     }
 }
