2c1b72e7b821628572febe3b06924f594dd8e359
Daljung <msfall@hanmail.net>
2023-10-22 11:50:02 +0900

    toast 에 변수처리까지 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.n2isys.loginuikotlintest_dal"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.n2isys.loginuikotlintest_dal"
--- app/src/main/java/com/n2isys/loginuikotlintest_dal/MainActivity.kt
+++ app/src/main/java/com/n2isys/loginuikotlintest_dal/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.n2isys.loginuikotlintest_dal
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.n2isys.loginuikotlintest_dal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,14 +14,14 @@ class MainActivity : AppCompatActivity() {
 
         viewBind.BtnLogin.setOnClickListener {
 
-            val ls_id = viewBind.TxtId.text
-            val ls_pwd = viewBind.TxtPwd.text
+            val ls_id = viewBind.TxtId.text.toString()
+            val ls_pwd = viewBind.TxtPwd.text.toString()
+            val ls_name = "달정".toString()
 
             if(ls_id == "admin@test.com" && ls_pwd == "qwer"){
-//                Toast.makeText("로그인성공", "", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "${ls_name} 관리자입니다.", Toast.LENGTH_SHORT).show()
             }else{
-//                Toast.makeText("로그인실패", "", Toast.LENGTH_SHORT).show()
-
+                Toast.makeText(this, "로그인실패입니다.", Toast.LENGTH_SHORT).show()
             }
 
 
